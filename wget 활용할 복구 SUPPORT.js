4feb6700bd9ed7b944481c10ba0b97ab24233fa5
voomin <42263574+voomin@users.noreply.github.com>
2019-02-16 10:31:39 +0900

    Add files via upload

---
+++ wget 활용할 복구 SUPPORT.js
@@ -0,0 +1,67 @@
+const domain="https://barotok.com/";
+
+var o=[
+    {
+        ext:'html',
+        arr:[
+            "header",
+            "footer",
+            "business",
+            "products",
+            "service",
+            "TermsOfService",
+            "FAQ",
+        ]
+    },
+    {
+        ext:'css',
+        arr:[
+            "business",
+            "products",
+            "service",
+            "TermsOfService",
+            "FAQ",
+        ]
+    },
+    {
+        ext:'js',
+        arr:[
+            "business",
+            "products",
+            "service",
+            "TermsOfService",
+            "FAQ",
+        ]
+    }
+]
+
+function wgetConsole(obj){
+    console.log(obj.ext+"------------ START ----------");
+    for(i in obj.arr){
+        var wget="wget -r -np "+domain+obj.ext+"/"+obj.arr[i]+"."+obj.ext;
+        console.log(wget);
+    }
+    if(obj.ext=='html'){
+        console.log("img------------ START ----------");
+        allImg(obj);
+    }
+}
+function allImg(htmlObj){
+    var imgs=[
+        "png",
+        "jpg",
+        "jpeg",
+    ]
+    for(i in htmlObj.arr){
+        obj=htmlObj
+        for(k in imgs){
+            var wget="wget -A \"*."+imgs[k]+"\" -r "+domain+obj.ext+"/"+obj.arr[i]+"."+obj.ext;
+            console.log(wget);
+        }
+    }
+
+}
+
+for(index in o){
+    wgetConsole(o[index]);
+}
